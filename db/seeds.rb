9b99b4ff2fe102d19c8f74eca625b71c929a95ac
Benjamin Leray <b.leray35@gmail.com>
2020-02-03 18:36:09 +0100

    Models OK, firt part of the seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Starting Seed"
+
+benjamin = User.create!(
+  email: "benjamin@retro-asphalt.com",
+  password: "password",
+  first_name: "Benjamin",
+  last_name: "Leray",
+  )
+
+toto = User.create!(
+  email: "toto@retro-asphalt.com",
+  password: "password",
+  first_name: "Toto",
+  last_name: "Dupont",
+  )
+
+puts "Fake Users done!"
+
+first_car = Car.create!(
+  brand: "Ford",
+  model: "GT 40",
+  power: 260,
+  price: 175,
+  year: 1956,
+  user_id: 1,
+  description: "Magnifique voiture, vous aurez l'impression de revoir la série Starky et Hutch et vous aurez envie de vous laisser pousser la moustache.",
+  )
+
+puts "Car done"
+
+puts "Seed done!"
