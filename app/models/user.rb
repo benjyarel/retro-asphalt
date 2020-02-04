360f16c15340ff2a225808c4f05714f4c9d95a12
Benjamin Leray <b.leray35@gmail.com>
2020-02-04 09:36:51 +0100

    fixed active storage migration, and creating routes for index/show cars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true
   validates :first_name, :last_name, length: { in: 3..25 }
+  has_one_attached :picture
 end
