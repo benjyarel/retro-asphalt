ceee4d4be91c4f2d2d2238e68f033bd97edb383f
Benjamin Leray <b.leray35@gmail.com>
2020-02-05 12:07:12 +0100

    first css for readibility in index done, with image tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def instance_image_tag(picture, options = {})
+    if picture.attached?
+      cl_image_tag picture.key, options
+    else
+      image_tag "no-picture.png", options
+    end
+  end
 end
