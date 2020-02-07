05b9c4db5d86b4b56d081bb91c101522c6d11554
Benjamin Leray <b.leray35@gmail.com>
2020-02-07 16:23:52 +0100

    simple form for edit ok

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -16,15 +16,21 @@ class CarsController < ApplicationController
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
     @car = Car.find(params[:id])
+    if @car.update(permit_cars_params)
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def permit_cars_params
-    params(:cars).permit(:brand, :model, :picture, :address, :power, :price)
+    params.require(:car).permit(:brand, :model, :picture, :address, :power, :price)
   end
 end
