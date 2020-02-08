e9fabc78965b7f0b9c93a26e18137898dd588099
Benjamin Leray <b.leray35@gmail.com>
2020-02-08 10:41:59 +0100

    installing test

--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -1,14 +1,18 @@
 require 'test_helper'
 
 class CarsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get cars_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   visit('/cars')
+  #   assert(true, " You can go to the index")
+  #   take_screenshot
+  #   assert_response :success
+  # end
 
-  test "should get show" do
-    get cars_show_url
-    assert_response :success
-  end
+  # test "should get show" do
+  #   visit('/cars/1')
+  #   assert(true, " You can go to the show")
+  #   take_screenshot
+  #   assert_response :success
+  # end
 
 end
