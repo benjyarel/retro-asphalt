13676a3e2effcf0f7b0a156e252953d5ab9702ec
Benjamin Leray <b.leray35@gmail.com>
2020-02-17 11:55:06 +0100

    ajax for new car ok in dashboard

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :user
-  #validates :brand, :model, :year, :price, :address, presence: true
-  #validates :description, length: { in: 30..500 }
+  validates :brand, :model, :year, :price, :address, presence: true
+  validates :description, length: { in: 30..500 }
   has_one_attached :picture
- # geocoded_by :address
- # after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
